a4de5f90b8b04176bd2006b61cb44443639c9eb8
fabian-elmar <fabsiban09@gmail.com>
2022-09-24 21:12:30 +0200

    add id

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -18,7 +18,7 @@ export default function Gallery(props) {
             <>
                 {memList.map((mem, i) => (
                     <div className="col-12 col-sm-6 col-md-4 g-3" key={i}>
-                        {console.log(i)}
+
 
                         <div className="card border-secondary mb-sm-3 mb-lg-5">
                             <img src={mem.imageURL} alt="Gallery-img-from-db" className="card-img-top" />
@@ -47,7 +47,7 @@ export default function Gallery(props) {
 
                 <div className="col-12 col-sm-6 col-md-4 g-3"></div>
                 <div className="card border-secondary mb-sm-3 mb-lg-5">
-                    <img src="https://tse3.mm.bing.net/th?id=OIP.rliCHkimK_S9BcNqiK4eDwHaF4" alt="Gallery-img-from-db" className="card-img-top" />
+                    <img src='https://tse3.mm.bing.net/th?id=OIP.rliCHkimK_S9BcNqiK4eDwHaF4' alt="Gallery-img-from-db" className="card-img-top" />
                     <div className="card-body">
                         <h5 className="card-title">{memList[1].name}</h5>
                         <p className="card-text">smth</p>
