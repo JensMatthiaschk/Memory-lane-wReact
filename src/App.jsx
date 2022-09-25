f350ed5919cfdc8fe5b3446bbc79c8c83b31f25b
fabian-elmar <fabsiban09@gmail.com>
2022-09-25 15:27:12 +0200

    use localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Container, Row, Col, Card } from "react-bootstrap";
 import InputForm from './InputForm'
 import Gallery from './Gallery'
 import Navbar from './Navbar'
-
+const Local_Storage_Key = "react_Memory_lane";
 
 function App() {
   const [memories, setMemories] = useState([
@@ -11,6 +11,18 @@ function App() {
     { title: "Lorem ipsum dolor2", name: "Pasqual Lunère", time: "1973-06-14", timePeriod: "", location: "Paris", memory: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta praesentium doloribus porro, reiciendis deserunt veniam ab dicta consequatur", imageURL: "https://footageforpro.com/wp-content/uploads/2019/08/1284Paris1973b.jpg" },
     { title: "Lorem ipsum dolor3", name: "Günther Riedel", time: "1952-04-22", timePeriod: "", location: "Berlin", memory: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta praesentium doloribus porro, reiciendis deserunt veniam ab dicta consequatur", imageURL: "http://www.dezimmer.net/Webalbum%20Berlin1952/slides/Berlin%201952%2020.JPG" }
   ])
+
+  useEffect(() => {
+    const storedMemories = JSON.parse(localStorage.getItem(Local_Storage_Key));
+    { setMemories(storedMemories) }
+  }
+    , [])
+
+  useEffect(() => {
+    if (memories.length > 3) localStorage.setItem(Local_Storage_Key, JSON.stringify(memories));
+
+  }, [memories])
+  console.log(Local_Storage_Key)
 
   const getData = (newMemoryStateFromForm) => {
     setMemories(memories => [...memories, ...newMemoryStateFromForm])
